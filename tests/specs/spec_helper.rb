0421530ee4dfa3cb5fd0ce26270887b7e6e48433
Mauricio Giraldo <mgiraldo@gmail.com>
2015-04-21 22:07:30 -0400

    Addes spec_helper

---
+++ tests/specs/spec_helper.rb
@@ -0,0 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'app'  # <-- your sinatra app
+require 'rspec'
+require 'rack/test'
+require 'lib/templates'
+require 'lib/pages'
+require 'lib/parser'
+require 'models/contents'
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
